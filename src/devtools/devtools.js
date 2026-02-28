da47a8c9b7288e78a708a513feec7b22a148590b
0xE31 <thethreeonee@gmail.com>
2026-02-28 09:44:19 +0800

    feat: migrate devtools panel to vite react antd dnd-kit

--- src/devtools/devtools.js
+++ src/devtools/devtools.js
@@ -1,6 +1,1 @@
-chrome.devtools.panels.create(
-  'Redirect Rules',
-  '',
-  'src/devtools/panel.html',
-  () => {}
-);
+chrome.devtools.panels.create('Redirect Rules', '', 'src/devtools/panel-bundle/index.html', () => {});
