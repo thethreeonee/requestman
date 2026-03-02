df2210ee5706ab737bc1398d60d5b22ebde852e4
0xE31 <thethreeonee@gmail.com>
2026-03-02 15:04:37 +0800

    fix: use table border style instead of bordered prop

--- src/redirect/components/RedirectRuleList.tsx
+++ src/redirect/components/RedirectRuleList.tsx
@@ -97,7 +97,6 @@ export default function RedirectRuleList({
     </div>
     <Table<TableRow>
       className="rules-list-table"
-      bordered
       pagination={false}
       dataSource={tableData}
       rowKey="key"
