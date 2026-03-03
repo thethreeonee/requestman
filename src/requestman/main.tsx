79e486a048cc3ab36348c354183a181fc78f32e2
0xE31 <thethreeonee@gmail.com>
2026-03-03 09:33:19 +0800

    refactor: rename RedirectPanel to RequestmanPanel

--- src/requestman/main.tsx
+++ src/requestman/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'antd';
 import 'antd/dist/reset.css';
 import './index.css';
-import RedirectPanel from './RedirectPanel';
+import RequestmanPanel from './RequestmanPanel';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App>
-      <RedirectPanel />
+      <RequestmanPanel />
     </App>
   </React.StrictMode>,
 );
