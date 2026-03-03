465e391dac88e60e4e6bce1d59b15a779aff3f11
0xE31 <thethreeonee@gmail.com>
2026-03-03 17:13:56 +0800

    style: 调整规则详情页规则类型字号并添加图标

--- src/requestman/components/RuleNameHeader.tsx
+++ src/requestman/components/RuleNameHeader.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { Button, Input, Space, Typography } from 'antd';
-import { EditOutlined } from '@ant-design/icons';
+import {
+  ApiOutlined,
+  ClockCircleOutlined,
+  CodeOutlined,
+  EditOutlined,
+  FileDoneOutlined,
+  FileSearchOutlined,
+  InsertRowAboveOutlined,
+  RetweetOutlined,
+  StopOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
 import { RULE_TYPE_LABEL_MAP } from '../constants';
 import type { RedirectRule } from '../types';
 
@@ -9,6 +20,18 @@ type Props = {
   editRuleName: boolean;
   setEditRuleName: React.Dispatch<React.SetStateAction<boolean>>;
   setWorkingRule: React.Dispatch<React.SetStateAction<RedirectRule | null>>;
+};
+
+const RULE_TYPE_ICON_MAP: Record<RedirectRule['type'], React.ReactNode> = {
+  redirect_request: <RetweetOutlined />,
+  rewrite_string: <CodeOutlined />,
+  query_params: <InsertRowAboveOutlined />,
+  modify_request_body: <FileSearchOutlined />,
+  modify_response_body: <FileDoneOutlined />,
+  modify_headers: <ApiOutlined />,
+  user_agent: <UserOutlined />,
+  cancel_request: <StopOutlined />,
+  request_delay: <ClockCircleOutlined />,
 };
 
 export default function RuleNameHeader({ rule, editRuleName, setEditRuleName, setWorkingRule }: Props) {
@@ -25,9 +48,14 @@ export default function RuleNameHeader({ rule, editRuleName, setEditRuleName, se
         ) : (
           <Typography.Title level={4} style={{ margin: 0 }}>{rule.name}</Typography.Title>
         )}
-        <Typography.Text type="secondary" style={{ fontSize: 12 }}>
-          {RULE_TYPE_LABEL_MAP[rule.type]}
-        </Typography.Text>
+        <Space size={6} align="center">
+          <Typography.Text type="secondary" style={{ fontSize: 14 }}>
+            {RULE_TYPE_ICON_MAP[rule.type]}
+          </Typography.Text>
+          <Typography.Text type="secondary" style={{ fontSize: 14 }}>
+            {RULE_TYPE_LABEL_MAP[rule.type]}
+          </Typography.Text>
+        </Space>
       </div>
       <Button type="text" icon={<EditOutlined />} onClick={() => setEditRuleName(true)} />
     </Space>
