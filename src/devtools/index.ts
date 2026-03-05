98e85029d3448173c883305bf31dcd551654d4ad
0xE31 <thethreeonee@gmail.com>
2026-03-05 20:08:30 +0800

    feat: 跟新标题

--- src/devtools/index.ts
+++ src/devtools/index.ts
@@ -1,7 +1,7 @@
 const isFirefox = typeof navigator !== 'undefined' && /firefox/i.test(navigator.userAgent);
 
 chrome.devtools.panels.create(
-  isFirefox ? 'Requestman' : '🔄 Requestman',
+  isFirefox ? 'Requestman' : '✴️ Requestman',
   '',
   isFirefox ? '/requestman/index.html' : 'requestman/index.html',
 );
